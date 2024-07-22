460f01b7b99427c44a23a1d756ecf8a46039bd0b
Raed <abuzaidraed13@gmail.com>
2024-07-22 04:11:17 -0400

    Added helper functions for database class

--- include/Inventory.hpp
+++ include/Inventory.hpp
@@ -26,13 +26,32 @@ public:
     std::shared_ptr<Item> queryDatabase(const std::string &barcode);
 
     // Add an Item to the inventory
-    void Inventory::addItem(std::shared_ptr<Item> item);
+    void addItem(const Item &item);
 
     // Remove an Item from the inventory
-    void removeItem(std::shared_ptr<Item> item);
+    void removeItem(const Item &item);
+
+    /**
+     * Set items map
+     * @param items a refrence to an items map
+    */
+    void setItems(const std::unordered_map<int, std::shared_ptr<Item>> &items);
+
+    /**
+     * @return refrence to items map
+    */
+    const std::unordered_map<int, std::shared_ptr<Item>> &getItems() const;
+
+    /**
+     * @return Pointer to Item Object
+    */
+    std::shared_ptr<Item> getItem(const int barcode) const;
 
     // Get Inventory size
     int getSize() const;
+
+    // Get total value
+    int getValue() const;
 
 private:
     std::unordered_map<int, std::shared_ptr<Item>> items_;
