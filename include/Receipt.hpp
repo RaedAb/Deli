30595ffe41aee90a9450c47dbfd761b7dc1d0969
Raed <abuzaidraed13@gmail.com>
2024-08-02 04:34:29 -0400

    Added function to extract text

--- include/Receipt.hpp
+++ include/Receipt.hpp
@@ -2,6 +2,8 @@
 #define RECEIPT_HPP_
 #include <string>
 #include <opencv2/opencv.hpp>
+#include <tesseract/baseapi.h>
+#include <leptonica/allheaders.h>
 
 class Receipt
 {
@@ -14,6 +16,15 @@ public:
      * @param imagePath path to the receipt image
      */
     void preprocessImage(const std::string &imagePath);
-};
 
+    /**
+     * @brief extracts text from the image.
+     *
+     * Takes the pre processed image and extracts text
+     *
+     * @param imagePath path to the receipt image
+     * @return text that was recognized
+     */
+    std::string extractText(const std::string &imagePath);
+};
 #endif
--- src/Receipt.cpp
+++ src/Receipt.cpp
@@ -7,9 +7,34 @@
  *
  * @param imagePath path to the receipt image
  */
-void preprocessImage(const std::string &imagePath)
+void Receipt::preprocessImage(const std::string &imagePath)
 {
     cv::Mat image = cv::imread(imagePath, cv::IMREAD_GRAYSCALE);
     cv::threshold(image, image, 0, 255, cv::THRESH_BINARY | cv::THRESH_OTSU);
-    cv::imwrite("preprocessed_receipt.png", image);
+    cv::imwrite("receipts/preprocessed/preprocessed_receipt.png", image);
+}
+
+/**
+ * @brief extracts text from the image.
+ *
+ * Takes the pre processed image and extracts text
+ *
+ * @param imagePath path to the receipt image
+ * @return text that was recognized
+ */
+std::string Receipt::extractText(const std::string &imagePath)
+{
+    tesseract::TessBaseAPI tess;
+    tess.Init(NULL, "eng", tesseract::OEM_LSTM_ONLY);
+    tess.SetPageSegMode(tesseract::PSM_AUTO);
+
+    PIX *image = pixRead(imagePath.c_str());
+    tess.SetImage(image);
+    char *outText = tess.GetUTF8Text();
+
+    std::string text(outText);
+    delete[] outText;
+    pixDestroy(&image);
+
+    return text;
 }
