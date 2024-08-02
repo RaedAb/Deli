bcb8dee70de1e2107f97c191d63973f2de0572e0
Raed <abuzaidraed13@gmail.com>
2024-08-02 03:44:11 -0400

    plan to work on store later

--- include/Store.hpp
+++ include/Store.hpp
@@ -1,14 +1,17 @@
 #ifndef STORE_HPP_
 #define STORE_HPP_
 
-#include "Inventory.hpp"
+#include "./Inventory.hpp"
+#include "./Database.hpp"
 
 class Store {
     public:
+        // put the store class on pause for now
 
 
     private:
         Inventory deli;
+        Database products;
 
 };
 
